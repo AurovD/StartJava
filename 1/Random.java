36e9ec4a2faed7f29d0879c51ba8558adc19b490
Dmitry <dmitry8891@mail.ru>
2018-09-26 23:11:29 +0300

    first commit

--- 1/Random.java
+++ 1/Random.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 public class Random{ 
 
    public static void main(String args[]){
+    System.setOut(new PrintStream(System.out, true, "Cp866"));
+    System.setErr(new PrintStream(System.err, true, "Cp866"));
+
       int a = 0; // Начальное значение диапазона - "от"
       int b = 100; // Конечное значение диапазона - "до"
       
       int random_number1 = a + (int) (Math.random() * b); // Генерация случ-го числа
-      System.out.println("Guess number!!!");
+      System.out.println("Угадай число!!!");
       
       int number = 0;
 
