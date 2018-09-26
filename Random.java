11029450772f6f3aac09f546a6b5746341959a3f
Dmitry <dmitry8891@mail.ru>
2018-09-26 17:53:47 +0300

    First Commit

---
+++ Random.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Random{ 
+
+   public static void main(String args[]){
+      int a = 0; // Начальное значение диапазона - "от"
+      int b = 100; // Конечное значение диапазона - "до"
+      
+      int random_number1 = a + (int) (Math.random() * b); // Генерация случ-го числа
+      System.out.println("Guess number!!!");
+      
+      int number = 0;
+
+      while (random_number1 != number) {
+        Scanner k = new Scanner(System.in);
+        System.out.print ("Input number: ");
+		number = k.nextInt();
+
+		if (number == random_number1) {
+			System.out.println ("Well done!!");
+		}
+		else if (number > random_number1) {
+			System.out.println ("The number is smaller.");
+		} 
+		else {
+			System.out.println ("The number is larger.");
+		}
+
+	}
+	System.out.print ("You have guessed.");
+   }
+}
