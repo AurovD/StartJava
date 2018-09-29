f9e1761b8c486bf384dfb1573396911a86db4de0
Dmitry <dmitry8891@mail.ru>
2018-09-29 20:02:23 +0300

    gj

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main (String [] args) { 
-        Scanner input = new Scanner(System.in);
+        Scanner input =new Scanner(System.in);
 
             System.out.print("Choose operation (+, -, *, /, ^, %): ");
             char operation = input.next().charAt(0);
