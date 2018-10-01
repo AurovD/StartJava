935328d0088eb1807133bc34a7c4660d47e670d5
Dmitry <dmitry8891@mail.ru>
2018-10-01 06:23:59 +0300

    format

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -35,7 +35,7 @@ public class Calculator {
 			        	result = number1 % number2; 
 			                System.out.println(result);
             		} else {
-            			System.out.println("Error!!!");
+            			System.out.println("Error!!");
             		}
         		}
     }
