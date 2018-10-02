d690c1c63960815b2cb8b1dff4d7762058164857
Dmitry <dmitry8891@mail.ru>
2018-10-02 20:19:06 +0300

    add

---
+++ Lesson1/Calculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner; 
+
+public class Calculator {
+    public static void main (String [] args) { 
+        Scanner input = new Scanner(System.in);
+            System.out.print("Input first numbers: ");
+            int number1 = input.nextInt();
+
+            System.out.print("Choose operation (+, -, *, /, ^, %): ");
+            char operation = input.next().charAt(0);
+            int result;
+
+        	System.out.print("Input second numbers: ");
+			int number2 = input.nextInt();
+            			
+            		if (operation == '+') {
+        	   			result = number1 + number2; 
+			               	System.out.println(result);
+			        } else if (operation == '-') {
+			        	result = number1 - number2; 
+			               	System.out.println(result);
+			        } else if (operation == '*') {
+			        	result = number1 * number2; 
+			              	System.out.println(result);
+			        } else if (operation == '/') {
+			        	result = number1 / number2;
+			               	System.out.println(result); 
+			        } else if (operation == '%') {
+			        	result = number1 % number2; 
+			                System.out.println(result);
+                    } else if (operation == '^') {
+                        result = number1 * number1; 
+                            System.out.println(result);
+                        result = number2 * number2; 
+                            System.out.println(result);
+                    } else {
+            			System.out.println("Error!!");
+            		}
+        		
+    }
+
+}
