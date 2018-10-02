7bb59d807265153c3c946e6c7862d3c3f2039bdc
Dmitry <dmitry8891@mail.ru>
2018-10-02 20:08:04 +0300

    описание изменений, которые вы внесли в программу

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -3,21 +3,15 @@ import java.util.Scanner;
 public class Calculator {
     public static void main (String [] args) { 
         Scanner input = new Scanner(System.in);
+            System.out.print("Input first numbers: ");
+            int number1 = input.nextInt();
 
             System.out.print("Choose operation (+, -, *, /, ^, %): ");
             char operation = input.next().charAt(0);
             int result;
 
-        		if (operation == '^') {
-        	    	System.out.print("Input number: ");
-		        	int number1 = input.nextInt();
-
-	        	    result = number1 * number1; 
-    	            System.out.println(result);
-           		} else {
-					System.out.print("Input 2 numbers: ");
-					int number1 = input.nextInt();
-					int number2 = input.nextInt();
+        	System.out.print("Input second numbers: ");
+			int number2 = input.nextInt();
             			
             		if (operation == '+') {
         	   			result = number1 + number2; 
@@ -34,10 +28,15 @@ public class Calculator {
 			        } else if (operation == '%') {
 			        	result = number1 % number2; 
 			                System.out.println(result);
+                    } else if (operation == '^') {
+                        result = number1 * number1; 
+                            System.out.println(result);
+                        result = number2 * number2; 
+                            System.out.println(result);
                     } else {
             			System.out.println("Error!!");
             		}
-        		}
+        		
     }
 
 }
