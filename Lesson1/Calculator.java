726ad963bc2ea433b03c3ac587c426e57147b390
Dmitry <dmitry8891@mail.ru>
2018-10-03 08:58:51 +0300

    format

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -4,39 +4,39 @@ public class Calculator {
     public static void main (String [] args) { 
         Scanner input = new Scanner(System.in);
             
-            System.out.print("Input first number: ");
-                int number1 = input.nextInt();
+        System.out.print("Input first number: ");
+        int number1 = input.nextInt();
 
-            System.out.print("Choose operation (+, -, *, /, ^, %): ");
-                char operation = input.next().charAt(0);
-                    int result;
+        System.out.print("Choose operation (+, -, *, /, ^, %): ");
+        char operation = input.next().charAt(0);
+        int result;
 
-            System.out.print("Input second number: ");
-                int number2 = input.nextInt();
+        System.out.print("Input second number: ");
+        int number2 = input.nextInt();
             			
-                if (operation == '+') {
-                    result = number1 + number2; 
-                        System.out.println(result);
-                } else if (operation == '-') {
-                    result = number1 - number2; 
-                        System.out.println(result);
-                } else if (operation == '*') {
-                    result = number1 * number2; 
-                        System.out.println(result);
-                } else if (operation == '/') {
-                    result = number1 / number2;
-                        System.out.println(result); 
-                } else if (operation == '%') {
-                    result = number1 % number2; 
-                        System.out.println(result);
-                } else if (operation == '^') {
-                    result = number1 * number1; 
-                        System.out.println(result);
-                    result = number2 * number2; 
-                        System.out.println(result);
-                } else {
-                System.out.println("Error!!");
-                }       		
+        if (operation == '+') {
+            result = number1 + number2; 
+            System.out.println(result);
+        } else if (operation == '-') {
+            result = number1 - number2; 
+            System.out.println(result);
+        } else if (operation == '*') {
+            result = number1 * number2; 
+            System.out.println(result);
+        } else if (operation == '/') {
+            result = number1 / number2;
+            System.out.println(result); 
+        } else if (operation == '%') {
+            result = number1 % number2; 
+            System.out.println(result);
+        } else if (operation == '^') {
+            result = number1 * number1; 
+            System.out.println(result);
+            result = number2 * number2; 
+            System.out.println(result);
+        } else {
+            System.out.println("Error!!");
+        }       		
     }
 
 }
--- Lesson1/Guess.java
+++ Lesson1/Guess.java
@@ -2,23 +2,23 @@ import java.util.Scanner;
 
 public class Guess { 
     public static void main(String args[]) {
-    	        Scanner input = new Scanner(System.in);
-		int randomNumber = 67;
-		int userNumber = 0;
+    	Scanner input = new Scanner(System.in);
+            int randomNumber = 67;
+            int userNumber = 0;
 
-		System.out.println("Guess number!!!");
-			while (randomNumber != number) {
-        		        System.out.print("Input number: ");
-				userNumber = input.nextInt();
+            System.out.println("Guess number!!!");
+            while (randomNumber != userNumber) {
+            System.out.print("Input number: ");
+		userNumber = input.nextInt();
 
-				if (number == randomNumber) {
-					System.out.println("Well done!!");
-				} else if (userNumber > randomNumber) {
-					System.out.println("The number is smaller.");
-				} else {
-					System.out.println("The number is larger.");
-				}
-			}
-		System.out.print ("You have guessed.");
+		if (userNumber == randomNumber) {
+                    System.out.println("Well done!!");
+		} else if (userNumber > randomNumber) {
+                    System.out.println("The number is smaller.");
+		} else {
+                    System.out.println("The number is larger.");
+		}
+            }
+            System.out.print ("You have guessed.");
    }
 }
