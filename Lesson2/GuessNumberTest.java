0b21752e6c63702b75ebd285c2e53ba2ff830766
Dmitry <dmitry8891@mail.ru>
2018-10-28 19:04:51 +0300

    new files

---
+++ Lesson2/GuessNumberTest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class GuessNumberTest {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        Player playerOne = new Player();
+        playerOne.setPlayerName();
+        Player playerTwo = new Jaeger();
+        playerTwo.setPlayerName();
+    }
+}
---
+++ Lesson2/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private String playerName;
+    
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    void setplayerName(String playerName) {
+        this.playerName = playerName;
+    }
+}
